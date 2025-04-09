0a5d0c3bf1b1dcda6efc49243411c6cfc479acc4
MrArkano69 <jorgeenriquebalagueracanas@gmail.com>
2025-04-09 17:25:10 -0500

    Perfil

---
+++ app/src/main/java/co/edu/unab/jorgebalaguera/storeapp/ui/theme/LoginScreen.kt
@@ -0,0 +1,98 @@
+package co.edu.unab.jorgebalaguera.storeapp.ui.theme
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+
+
+@Preview
+@Composable
+fun LoginScreen() {
+    Scaffold { innerPadding ->
+
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 24.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                imageVector = Icons.Default.Person,
+                contentDescription = null,
+                modifier = Modifier.size(150.dp),
+                colorFilter = ColorFilter.tint(Color(0xFFFF9900))
+            )
+
+            Text(
+                text = "Iniciar Sesión",
+                color = Color(0xFFFF9900),
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(Icons.Default.Email, contentDescription = null)
+                },
+                label = {
+                    Text("Correo Electrónico")
+                }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(Icons.Default.Lock, contentDescription = null)
+                },
+                label = {
+                    Text("Contraseña")
+                },
+                visualTransformation = PasswordVisualTransformation()
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF9900)),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Iniciar Sesión", color = Color.White)
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+
+            TextButton(onClick = {}) {Text(
+                text = "¿No tienes cuenta? Registrate.",
+                color = Color(0xFFFF9900)
+            )
+
+            }
+        }
+    }
+}
